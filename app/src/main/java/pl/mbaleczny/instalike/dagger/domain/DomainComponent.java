f87926eb4890491ee5f73544a0f65c758d69c39e
mbaleczny <mariuszbaleczny@gmail.com>
2017-05-06 14:04:58 +0200

    Swap scheduler method with dataSource

--- app/src/main/java/pl/mbaleczny/instalike/dagger/domain/DomainComponent.java
+++ app/src/main/java/pl/mbaleczny/instalike/dagger/domain/DomainComponent.java
@@ -3,11 +3,11 @@ package pl.mbaleczny.instalike.dagger.domain;
 import dagger.Component;
 import pl.mbaleczny.instalike.dagger.app.AppComponent;
 import pl.mbaleczny.instalike.dagger.scope.DomainScope;
-import pl.mbaleczny.instalike.util.scheduler.BaseSchedulerProvider;
+import pl.mbaleczny.instalike.domain.DataSource;
 
 @DomainScope
 @Component(dependencies = AppComponent.class, modules = DomainModule.class)
 public interface DomainComponent {
 
-    BaseSchedulerProvider scheduler();
+    DataSource dataSource();
 }
