38ed677a1ff47927984847995ac6d0bc3e28e640
mbaleczny <mariuszbaleczny@gmail.com>
2017-05-07 19:50:46 +0200

    Make Parcelable CommentsCount and LikesCount

--- app/src/main/java/pl/mbaleczny/instalike/domain/model/CommentsCount.java
+++ app/src/main/java/pl/mbaleczny/instalike/domain/model/CommentsCount.java
@@ -1,9 +1,31 @@
 package pl.mbaleczny.instalike.domain.model;
 
-public class CommentsCount {
+import android.os.Parcel;
+import android.os.Parcelable;
 
+public class CommentsCount implements Parcelable {
+
+    public static final Parcelable.Creator<CommentsCount> CREATOR = new Parcelable.Creator<CommentsCount>() {
+        @Override
+        public CommentsCount createFromParcel(Parcel source) {
+            return new CommentsCount(source);
+        }
+
+        @Override
+        public CommentsCount[] newArray(int size) {
+            return new CommentsCount[size];
+        }
+    };
     private long imageId;
     private int comments;
+
+    public CommentsCount() {
+    }
+
+    protected CommentsCount(Parcel in) {
+        this.imageId = in.readLong();
+        this.comments = in.readInt();
+    }
 
     public long getImageId() {
         return imageId;
@@ -19,5 +41,16 @@ public class CommentsCount {
 
     public void setComments(int comments) {
         this.comments = comments;
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeLong(this.imageId);
+        dest.writeInt(this.comments);
     }
 }
--- app/src/main/java/pl/mbaleczny/instalike/domain/model/LikesCount.java
+++ app/src/main/java/pl/mbaleczny/instalike/domain/model/LikesCount.java
@@ -1,9 +1,31 @@
 package pl.mbaleczny.instalike.domain.model;
 
-public class LikesCount {
+import android.os.Parcel;
+import android.os.Parcelable;
 
+public class LikesCount implements Parcelable {
+
+    public static final Parcelable.Creator<LikesCount> CREATOR = new Parcelable.Creator<LikesCount>() {
+        @Override
+        public LikesCount createFromParcel(Parcel source) {
+            return new LikesCount(source);
+        }
+
+        @Override
+        public LikesCount[] newArray(int size) {
+            return new LikesCount[size];
+        }
+    };
     private long imageId;
     private int likes;
+
+    public LikesCount() {
+    }
+
+    protected LikesCount(Parcel in) {
+        this.imageId = in.readLong();
+        this.likes = in.readInt();
+    }
 
     public long getImageId() {
         return imageId;
@@ -19,5 +41,16 @@ public class LikesCount {
 
     public void setLikes(int likes) {
         this.likes = likes;
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeLong(this.imageId);
+        dest.writeInt(this.likes);
     }
 }
